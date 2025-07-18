92fee468c3015f18ed7c3556818efb4f56d698d7
Pete Richards <pete.richards@ksmcpa.com>
2025-07-18 09:30:35 -0600

    Remove local file test from repo

--- test_excel_to_json.py
+++
@@ -1,5 +0,0 @@
-from utils.excel_utils import excel_to_json
-
-records, columns = excel_to_json("C:\\Users\\pete.richards\\OneDrive - KSM Business Services Inc\\Desktop\\KSM Trial Balance 3.31.2025.xlsx")
-print("Detected columns:", columns)
-print("First record sample:", records[0])
