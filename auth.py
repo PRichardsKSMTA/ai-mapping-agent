a89611294b5e392e5d04e8e4e3c84f9e05e6dfa5
Pete Richards <pete.richards@ksmcpa.com>
2025-08-19 16:52:49 -0600

    Center the login button

--- auth.py
+++ auth.py
@@ -207,7 +207,7 @@ else:
         )
 
         # Create three columns and render the login component in the center one
-        left, center, right = st.columns([1, 1.1, 1])
+        left, center, right = st.columns([1, 1.3, 1])
         token = None
         with center:
             token = msal_authentication(
